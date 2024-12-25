8f752abe36c6c38ab6b9241930c6c705b8be4d95
Luai Malek <luai.malek@gmail.com>
2024-12-25 16:59:58 +0100

    Added locale support for keyboard inputs

--- src/ux/mod.rs
+++ src/ux/mod.rs
@@ -1,4 +1,30 @@
-use bevy::{prelude::*, utils::HashMap};
+// OmniCAD
+// A multi-domain CAD tool, written in Rust.
+//
+// Copyright (C) 2024 Luai "TheBeautifulOrc" Malek
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use bevy::{
+    input::{
+        keyboard::{Key, KeyboardInput},
+        ButtonState,
+    },
+    prelude::*,
+    utils::HashMap,
+};
+use std::fmt::Debug;
 
 pub struct UXPlugin;
 
@@ -7,36 +33,65 @@ impl Plugin for UXPlugin {
         app.add_event::<InputEvent>();
         app.add_systems(Update, input_handler);
         app.insert_resource(Mode::default());
+        app.insert_resource(ModifierKeys::new());
     }
 }
+
 #[derive(Event)]
-struct InputEvent(KeyCode);
+struct InputEvent {
+    key: Key,
+    modifiers: ModifierKeys,
+}
+impl Debug for InputEvent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("InputEvent")
+            .field("key", &format!("{:?}", self.key))
+            .field("modifiers", &format!("{:?}", self.modifiers.0))
+            .finish()
+    }
+}
 
 fn input_handler(
-    mut ev_writer: EventWriter<InputEvent>,
-    keys: Res<ButtonInput<KeyCode>>,
-    mut viewports: Query<(&Activity, &mut Mode)>,
+    mut event_writer: EventWriter<InputEvent>,
+    mut key_event_reader: EventReader<KeyboardInput>,
+    mut modifier_keys: ResMut<ModifierKeys>,
 ) {
-    for key in keys.get_just_pressed() {
-        ev_writer.send(InputEvent(key.clone()));
+    for key_event in key_event_reader.read() {
+        // Track modifier keys
+        if let Some(modifier_count) = modifier_keys.0.get_mut(&key_event.logical_key) {
+            *modifier_count += match key_event.state {
+                ButtonState::Pressed => 1,
+                ButtonState::Released => -1,
+            };
+        }
+        // Send signale for all other keys
+        else if key_event.state == ButtonState::Pressed {
+            event_writer.send(InputEvent {
+                key: key_event.logical_key.clone(),
+                modifiers: modifier_keys.as_ref().clone(),
+            });
+        }
     }
 }
-// fn event_sender(mut ev_test: EventWriter<TestEvent>, keys: Res<ButtonInput<KeyCode>>) {
-//     for key in keys.get_just_pressed() {
-//         ev_test.send(TestEvent(key.clone()));
-//     }
-// }
-// fn event_receiver(mut ev_test: EventReader<TestEvent>) {
-//     for ev in ev_test.read() {
-//         println!("Event with payload {:?} received", ev.0);
-//     }
-// }
+
+#[derive(Resource, Clone)]
+pub struct ModifierKeys(HashMap<Key, i8>);
+
+impl ModifierKeys {
+    fn new() -> Self {
+        let mut map: HashMap<Key, i8> = HashMap::new();
+        map.insert(Key::Shift, 0);
+        map.insert(Key::Control, 0);
+        map.insert(Key::Alt, 0);
+        ModifierKeys(map)
+    }
+}
 
 #[derive(Resource, Component)]
 pub struct Mode {
     name: &'static str,
     // TODO: Change string for enum-based approach for keymap
-    keymap: HashMap<KeyCode, &'static str>,
+    keymap: HashMap<InputEvent, &'static str>,
 }
 impl Default for Mode {
     fn default() -> Self {
